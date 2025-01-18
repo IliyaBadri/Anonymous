b86060a745b89c43bc12c899408f74c94f96e46f
Iliya Badri <133355601+IliyaBadri@users.noreply.github.com>
2025-01-18 18:30:19 +0330

    Added project
    Added the initial state of the project to the git.
    
    - A few things are made like the database system and master password entry for the first time.

---
+++ Debug/DebugManager.cs
@@ -0,0 +1,31 @@
+﻿namespace Anonymous.Debug
+{
+    public class DebugManager
+    {
+        public static void LogException(Exception exception)
+        {
+            if (exception == null) {
+                return;
+            }
+
+            string report = "Exception log:\n-----BEGIN ANONYMOUS ERROR-----\nId (HRESULT): " + exception.HResult.ToString() + "\nMessage: " + exception.Message;
+
+            if (exception.Source != null) {
+                report += "\nSource: " + exception.Source;
+            }
+
+            if (exception.HelpLink != null) {
+                report += "\nHelp link: " + exception.HelpLink;
+            }
+
+            if (exception.StackTrace != null) {
+                report += "\nStack trace:\n" + exception.StackTrace;
+            }
+
+            report += "\n-----END ANONYMOUS ERROR-----";
+
+            Console.WriteLine(report);
+        
+        }
+    }
+}
---
+++ MainPage.xaml.cs
@@ -0,0 +1,114 @@
+﻿using Anonymous.Database;
+using Anonymous.Debug;
+using Anonymous.Security;
+
+namespace Anonymous
+{
+    public partial class MainPage : ContentPage
+    {
+        private bool accountPresent = false;
+        public MainPage()
+        {
+            InitializeComponent();
+            InitializeFunctionality();
+        }
+
+        private async void InitializeFunctionality()
+        {
+            try
+            {
+                PasswordFrame.IsEnabled = false;
+                PasswordEntry.IsEnabled = false;
+                AuthButton.IsEnabled = false;
+                await Task.Run(() =>
+                {
+                    DatabaseManager.InitializeTables();
+                    bool isAccountPresent = AccountDataManager.GetEncryptedAccount() != null;
+                    MainThread.BeginInvokeOnMainThread(() =>
+                    {
+                        if (isAccountPresent)
+                        {
+                            accountPresent = true;
+                            PasswordLabel.Text = "Please enter your master password";
+
+                        }
+                        else
+                        {
+                            accountPresent = false;
+                            PasswordLabel.Text = "Please enter a new master password";
+                        }
+                        PasswordEntry.IsEnabled = true;
+                        AuthButton.IsEnabled = true;
+                        PasswordFrame.IsEnabled = true;
+                    });
+                });
+            }
+            catch (Exception exception)
+            {
+                DebugManager.LogException(exception);
+            }
+        }
+
+        private async void OnAuthButtonClicked(object sender, EventArgs e)
+        {
+            try
+            {
+                PasswordFrame.IsEnabled = false;
+                PasswordEntry.IsEnabled = false;
+                AuthButton.IsEnabled = false;
+                if (accountPresent)
+                {
+                    return;
+                }
+                else
+                {
+                    bool accepted = await DisplayAlert("Attention!", "Please note that if you lose your password, there will be NO WAY to retrieve it. Make sure to store it securely. Do you want to proceed?", "YES", "NO");
+                    if (!accepted)
+                    {
+                        PasswordFrame.IsEnabled = true;
+                        PasswordEntry.IsEnabled = true;
+                        AuthButton.IsEnabled = true;
+                        return;
+                    }
+
+                    string? masterPassword = PasswordEntry.Text;
+
+                    if (masterPassword == null || masterPassword == "")
+                    {
+                        await DisplayAlert("Not acceptable!", "Your have to enter a new master password.", "OK");
+                        PasswordFrame.IsEnabled = true;
+                        PasswordEntry.IsEnabled = true;
+                        AuthButton.IsEnabled = true;
+                        return;
+                    }
+
+                    if (masterPassword.Length < 4)
+                    {
+                        await DisplayAlert("Not acceptable!", "Your master password must at least be 4 characters long.", "OK");
+                        PasswordFrame.IsEnabled = true;
+                        PasswordEntry.IsEnabled = true;
+                        AuthButton.IsEnabled = true;
+                        return;
+                    }
+
+                    await Task.Run(() =>
+                    {
+                        AccountDataManager.MakeNewAccount(masterPassword);
+                        ApplicationAuth.SetMasterPassword(masterPassword);
+                    });
+                }
+            } catch (Exception exception)
+            {
+                DebugManager.LogException(exception);
+                string error = "An error occured.\nError code: " + exception.HResult.ToString() + "\nDetails: " + exception.Message;
+                await DisplayAlert("Error!", error, "OK");
+            } finally
+            {
+                PasswordFrame.IsEnabled = true;
+                PasswordEntry.IsEnabled = true;
+                AuthButton.IsEnabled = true;
+            }
+        }
+    }
+
+}
